48fe6e8ceb91d859af936dca4eadee74ab1d5e69
any-stone <kuxa.business@gmail.com>
2021-08-14 19:32:02 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/switches.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/switches.dir/main.cpp.o"
+  "switches"
+  "switches.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/switches.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
